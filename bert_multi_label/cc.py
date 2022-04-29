1b1d2fab87c3350e25d994c4e8c8c20fe139dfda
曾庆学 <3141929594@qq.com>
2022-04-29 11:20:54 +0800

    添加了全局步和损失的打印

---
+++ bert_multi_label/cc.py
@@ -0,0 +1,9 @@
+#!/user/bin/env python
+# -*- coding: utf-8 -*-
+'''
+@project : exam_annotation
+@author  : zengqingxue
+#@file   : cc.py
+#@ide    : PyCharm
+#@time   : 2022-04-27 17:45:58
+'''  
---
+++ dd.py
@@ -0,0 +1,9 @@
+#!/user/bin/env python
+# -*- coding: utf-8 -*-
+'''
+@project : exam_annotation
+@author  : zengqingxue
+#@file   : dd.py
+#@ide    : PyCharm
+#@time   : 2022-04-27 18:35:31
+'''  
