9ada0ef3080d3c900f7f8abfcce3702456ec3056
曾庆学 <3141929594@qq.com>
2022-04-29 16:13:59 +0800

    测试 max_seq_length 128

--- bert_multi_label/run.sh
+++ bert_multi_label/run.sh
@@ -9,7 +9,7 @@ python run_classifier.py \
   --vocab_file pretrained_model/chinese_L-12_H-768_A-12/vocab.txt \
   --bert_config_file pretrained_model/chinese_L-12_H-768_A-12/bert_config.json \
   --init_checkpoint pretrained_model/chinese_L-12_H-768_A-12/bert_model.ckpt \
-  --max_seq_length 400 \
+  --max_seq_length 128 \
   --train_batch_size 32 \
   --learning_rate 3e-5 \
   --num_train_epochs 3.0 \
